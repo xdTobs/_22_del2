87cdd87c718f1e86aca13bda4df280e89663baa0
amanda <amanda.dyrby.andersen@gmail.com>
2022-10-12 13:03:13 +0200

    DiceGame class

--- src/main/java/DiceCup.java
+++ src/main/java/DiceCup.java
@@ -1,2 +1,28 @@
 public class DiceCup {
+
+    Die[] diceArray;
+
+    //returns the total sum of all dice
+    public int getSum(){
+        int sum = 0;
+        for(Die d : diceArray){
+            sum += d.getFaceValue();
+        }
+        return sum;
+    }
+
+    //rolls all dice
+    public void roll(){
+        for(Die d : diceArray){
+            d.roll();
+        }
+    }
+
+    //int dice defined in Main as the total number of dice in a cup
+    public DiceCup(int dice){
+        diceArray = new Die[dice];
+        for (int i = 0; i < dice; i++) {
+            diceArray[i] = new Die();
+        }
+    }
 }
