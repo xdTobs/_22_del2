582d07f164081ffcbeaf8b2faa7f29e163994330
xdTobs <70943428+xdTobs@users.noreply.github.com>
2022-10-25 15:13:53 +0200

    added more negative and large tests

--- src/test/java/DiceCupTest.java
+++ src/test/java/DiceCupTest.java
@@ -16,7 +16,7 @@ class DiceCupTest {
     }
 
     @Test
-    void getSum() {
+    void getSumStandard() {
         int res =0;
        int[] faceValues = diceCup.getFaceValueArray();
         for (int i : faceValues)res +=i;
@@ -24,12 +24,45 @@ class DiceCupTest {
     }
 
     @Test
+    void getSumNegative() {
+        diceCup = new DiceCup(-8);
+        int res =0;
+        int[] faceValues = diceCup.getFaceValueArray();
+        for (int i : faceValues)res +=i;
+        assertEquals(2,res);
+    }
+    @Test
+    void getSumLarge() {
+        diceCup = new DiceCup(1000);
+        int res =0;
+        int[] faceValues = diceCup.getFaceValueArray();
+        for (int i : faceValues)res +=i;
+        assertEquals(diceCup.getSum(),res);
+    }
+
+
+    @Test
     void roll() {
         //not used since it just calls roll on dice, which is tested
     }
 
     @Test
-    void getFaceValueArray() {
+    void getFaceValueArrayStandard() {
+        int res =0;
+        int[] faceValues = diceCup.getFaceValueArray();
+        for (int i : faceValues)res +=i;
+        assertEquals(res,diceCup.getSum());
+    }
+    @Test
+    void getFaceValueArrayNegative() {
+        diceCup = new DiceCup(-8);
+        int res =0;
+        int[] faceValues = diceCup.getFaceValueArray();
+        assertArrayEquals(new int[]{1, 1},faceValues);
+    }
+    @Test
+    void getFaceValueArrayLarge() {
+        diceCup = new DiceCup(1000);
         int res =0;
         int[] faceValues = diceCup.getFaceValueArray();
         for (int i : faceValues)res +=i;
